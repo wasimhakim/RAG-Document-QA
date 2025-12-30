267d1a591410fd11f3d414d11df92d5166d8b390
wasimhakim <wasimhakim89@gmail.com>
2025-12-30 16:04:02 +0400

    Root and health endpoints

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello World!")
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
+app = FastAPI()
+
+@app.get("/")
+def redirect_to_health():
+  return RedirectResponse(url="/health")
+
+@app.get("/health")
+async def root():
+  return { "message": "Hello I am working :)"}
